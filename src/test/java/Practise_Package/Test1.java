013f67a750686808e106994e2d6d4bd6041d9dcd
DELL <DELL@DESKTOP-7SNL7GR>
2022-12-26 00:24:35 +0530

    Merge branch 'master' of https://github.com/Rajputjayant/Qspvirtual.git
    Fourth Commit by eng 2

--- src/test/java/Practise_Package/Test1.java
+++ src/test/java/Practise_Package/Test1.java
@@ -10,6 +10,7 @@ public class Test1 {
 		System.out.println("This is demo");
 		System.out.println("This is not demo");
 		System.out.println("This is good demo");
+		System.out.println("This is bad demo");
 	}
 	@Test
 	public void call()
